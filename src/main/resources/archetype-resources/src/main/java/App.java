9cf6e1e6df8623680a09cf08f6fe8bf150e118fd
Godbell <12191579@inha.edu>
2025-01-10 16:57:07 +0900

    initial

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.poscodx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
